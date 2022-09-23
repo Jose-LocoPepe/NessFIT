e34b69fa5c8d9ecdabc6e625b875099c81a9b1a3
José Bautista <pepelota@MacBook-del-Loco-Pepe.local>
2022-09-23 18:47:21 -0300

    prueba

---
+++ src/main/java/cl/nessfit/web/controller/HomeController.java
@@ -0,0 +1,15 @@
+package cl.nessfit.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value = "/",method = RequestMethod.GET)
+	public String index() {
+		return "index";
+		
+	}
+
+}
--- src/main/java/cl/nessfit/web/service/UsuarioService.java
+++ src/main/java/cl/nessfit/web/service/UsuarioService.java
@@ -3,30 +3,31 @@ package cl.nessfit.web.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import cl.nessfit.web.model.Usuario;
 import cl.nessfit.web.repository.IUsuarioRepository;
 
+@Service
 public class UsuarioService implements IUsuarioService {
 
 	@Autowired
 	private IUsuarioRepository usuarioRepository;
 	@Override
 	public void guardar(Usuario usuario) {
-		// TODO Auto-generated method stub
+		usuarioRepository.save(usuario);
 		
 	}
 
 	@Override
 	public void eliminar(String rutUsuario) {
-		// TODO Auto-generated method stub
+		usuarioRepository.deleteById(rutUsuario);
 		
 	}
 
 	@Override
 	public List<Usuario> listar() {
-		// TODO Auto-generated method stub
-		return null;
+		return usuarioRepository.findAll();
 	}
 
 }
