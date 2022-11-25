2be5464d5f8f39754372865e397af1afb0e30690
Jose Bautista <103233618+Jose-LocoPepe@users.noreply.github.com>
2022-11-25 12:41:22 -0300

    Xd

--- src/main/java/cl/nessfit/web/ProyectoNessfitApplication.java
+++ src/main/java/cl/nessfit/web/ProyectoNessfitApplication.java
@@ -11,8 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 public class ProyectoNessfitApplication {
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoNessfitApplication.class, args);
 	}
